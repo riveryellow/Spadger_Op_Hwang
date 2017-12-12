24099944e5b88514f1b7d3af02566ec10e62ae0f
yellow_river <river_yellow@163.com>
2017-12-12 18:29:11 +0800

    menutree

---
+++ config/menuTree.js
@@ -0,0 +1,177 @@
+'use strict';
+
+module.exports = [
+  {
+    href: '#',
+    name: '游戏管理',
+    className: 'fa-dashboard',
+    subMenu: [
+      {
+        href: '',
+        name: '武汉麻将',
+      }
+    ],
+  },
+  {
+    href: '#',
+    name: '计费管理',
+    className: 'fa-files-o',
+    subMenu: [
+      {
+        href: '/index',
+        name: '用户设置',
+      },
+      {
+        href: '/index2',
+        name: '店铺设置',
+      },
+    ],
+  },
+  {
+    href: '#',
+    name: '订单管理',
+    className: 'fa-share',
+    subMenu: [
+      {
+        href: '#',
+        name: '第二层',
+        subMenu: [
+          {
+            href: '/index',
+            name: '第三层',
+          },
+          {
+            href: '/index2',
+            name: '第三层',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    href: '#',
+    name: '用户管理',
+    className: 'fa-share',
+    subMenu: [
+      {
+        href: '#',
+        name: '第二层',
+        subMenu: [
+          {
+            href: '/index',
+            name: '第三层',
+          },
+          {
+            href: '/index2',
+            name: '第三层',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    href: '#',
+    name: '活动管理',
+    className: 'fa-share',
+    subMenu: [
+      {
+        href: '#',
+        name: '第二层',
+        subMenu: [
+          {
+            href: '/index',
+            name: '第三层',
+          },
+          {
+            href: '/index2',
+            name: '第三层',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    href: '#',
+    name: '代理管理',
+    className: 'fa-share',
+    subMenu: [
+      {
+        href: '#',
+        name: '第二层',
+        subMenu: [
+          {
+            href: '/index',
+            name: '第三层',
+          },
+          {
+            href: '/index2',
+            name: '第三层',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    href: '#',
+    name: '数据统计',
+    className: 'fa-share',
+    subMenu: [
+      {
+        href: '#',
+        name: '第二层',
+        subMenu: [
+          {
+            href: '/index',
+            name: '第三层',
+          },
+          {
+            href: '/index2',
+            name: '第三层',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    href: '#',
+    name: '消息管理',
+    className: 'fa-share',
+    subMenu: [
+      {
+        href: '#',
+        name: '第二层',
+        subMenu: [
+          {
+            href: '/index',
+            name: '第三层',
+          },
+          {
+            href: '/index2',
+            name: '第三层',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    href: '#',
+    name: '权限管理',
+    className: 'fa-share',
+    subMenu: [
+      {
+        href: '#',
+        name: '第二层',
+        subMenu: [
+          {
+            href: '/index',
+            name: '第三层',
+          },
+          {
+            href: '/index2',
+            name: '第三层',
+          },
+        ],
+      },
+    ],
+  }
+];
